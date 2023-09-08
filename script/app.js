ab4c60733d683edfd0c849250bcfa3a64336fded
kambalong <74581988+kambalong@users.noreply.github.com>
2023-09-08 10:10:25 +0800

    Update app.js

--- script/app.js
+++ script/app.js
@@ -3,14 +3,18 @@ function displayProduct() {
   let secondNum = Number(document.getElementById('secondNum').innerHTML)
 
   let total = firstNum * secondNum;
-  document.getElementById("result").innerHTML = ` ${firstNum} + ${secondNum}, equals to ${total}` ;
+  document.getElementById("prodResult").innerHTML = ` ${firstNum} + ${secondNum}, equals to ${total}` ;
 }
 
 
 function displayDiff() {
 
+  let firstNum = Number(document.getElementById('firstNum').innerHTML)
+  let secondNum = Number(document.getElementById('secondNum').innerHTML)
 
+   let total = firstNum - secondNum;
   
+  document.getElementById("diffResult").innerHTML = ` ${firstNum} + ${secondNum}, equals to ${total}` ;
 }
 
   
